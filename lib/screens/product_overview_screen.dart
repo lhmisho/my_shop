a1956daef7cb83184f7f667b4b99e704aef87089
lhmisho <lhmisho@gmail.com>
2020-02-15 22:13:35 +0600

    product model and overview screen

---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductOverview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Let's build a shop"),
+      ),
+    );
+  }
+}
